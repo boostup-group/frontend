0541dc185c18a95f0d9bd000dd62764249b2cfbe
xBouzaieni <samirbouzaieni@gmail.com>
2024-09-27 22:43:32 +0100

    fix: resolve missing UI component imports and correct paths x6

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  redirects: () => {
-    return [
-      {
-        source: "/",
-        destination: "/en",
-        permanent: true,
-      },
-    ];
-  },
+  // Use the appropriate output based on your environment
+  output: 'development' === 'production' ? 'export' : 'standalone',
+
   env: {
     API_PROD_URL: "https://api.netflixtunisie.com/api/",
     // API_PROD_URL: "https://laravel.pixelstrap.net/fastkart/api/",
@@ -16,6 +10,7 @@ const nextConfig = {
     PAYMENT_CANCEL_URL: "http://204.216.214.207:3000/",
   },
   images: {
+    unoptimized: process.env.NODE_ENV === 'production', // Disable optimization for production
     remotePatterns: [
       {
         protocol: "https",
@@ -39,6 +34,16 @@ const nextConfig = {
         pathname: "**",
       }
     ],
+  },
+  // Add your redirects here, if needed
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
   },
 };
 
