040b137c8919af5c5ddc3e4a4db83c969c5beda4
xBouzaieni <samirbouzaieni@gmail.com>
2024-09-27 22:12:59 +0100

    fix: resolve missing UI component imports and correct paths x3

--- src/app/[lng]/(mainLayout)/[blogSlug]/page.js
+++ src/app/[lng]/(mainLayout)/[blogSlug]/page.js
@@ -1,14 +1,33 @@
-import SingleBlog from '@/Components/Blogs/SingleBlog'
+import SingleBlog from '@/Components/Blogs/SingleBlog';
+
+// This function tells Next.js which dynamic routes should be statically generated
+export async function generateStaticParams() {
+  // Fetch all possible blog slugs from the API
+  const blogSlugs = await fetch(`${process.env.API_PROD_URL}blog/slugs`)
+    .then(res => res.json())
+    .catch(err => console.log('Error fetching slugs', err));
+
+  // Return the list of static params for Next.js to generate pages
+  return blogSlugs?.map(slug => ({
+    blogSlug: slug
+  }));
+}
+
+// Fetch metadata for each blog
 export async function generateMetadata({ params }) {
-  // fetch data
-  const blogData = await fetch(`${process.env.API_PROD_URL}blog/slug/${params?.blogSlug}`).then((res) => res.json()).catch((err) => console.log("err", err))
+  // Fetch blog data using the slug from the params
+  const blogData = await fetch(`${process.env.API_PROD_URL}blog/slug/${params?.blogSlug}`)
+    .then(res => res.json())
+    .catch(err => console.log("Error fetching blog data", err));
+
+  // Return metadata for the blog page
   return {
     openGraph: {
       title: blogData?.meta_title,
       description: blogData?.meta_description,
       images: [blogData?.blog_meta_image?.original_url, []],
     },
-  }
+  };
 }
 
 const BlogDetailContent = ({ params }) => {
@@ -16,7 +35,7 @@ const BlogDetailContent = ({ params }) => {
     <>
       {params && <SingleBlog params={params?.blogSlug} />}
     </>
-  )
-}
+  );
+};
 
-export default BlogDetailContent
+export default BlogDetailContent;
