b8c61b36c66e043a6ce82eb706fddd02fd100db7
xBouzaieni <samirbouzaieni@gmail.com>
2024-10-22 11:57:42 +0100

    update

--- src/Components/Giveaway/index.jsx
+++ src/Components/Giveaway/index.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "../ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
 
 export default function GiveawayPage() {
     return (
